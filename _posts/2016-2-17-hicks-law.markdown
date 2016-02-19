4ba0089bcc80a86600083c128217ff74a45b8d77
Matti Salokangas <matti.salokangas@gmail.com>
2016-02-18 20:23:12 -0800

    Design updates

---
+++ _posts/2016-2-17-hicks-law.markdown
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "Hicks Law"
+date:   2016-2-17 15:58:29 -0800
+categories: ux
+background: '8.jpg'
+---
+
+## Only show what is absolutely necessary
+
--- js/main.js
+++ js/main.js
@@ -30,9 +30,49 @@
   $(init);
 
   function init() {
-    $('.js-site-nav-trigger').on('click', function() {
-      $('.js-site-nav-container,.js-site-nav-trigger').toggleClass('active');
-    });
+    $('.js-site-nav-trigger')
+      .on('click', function() {
+        $('.js-site-nav-container,.js-site-nav-trigger').toggleClass('active');
+      });
+
+    // var maxAnimations = 30;
+    // var animationCount = 0;
+    // $('.site-logo')
+    //   .on('mouseover', _.debounce(function() {
+
+    //     if (animationCount === maxAnimations) {
+    //       $('.site-logo').off('mouseover');
+    //       return false;
+    //     }
+
+    //     $('.site-logo svg').velocity({
+    //       translateX: '175px',
+    //       rotateZ: '1440deg'
+    //     }, 500);
+
+    //     $('.site-title').velocity({
+    //       left: '-42px'
+    //     }, 500);
+
+    //     animationCount++;
+    //   }, 300))
+    //   .on('mouseout', _.debounce(function() {
+    //     if (animationCount === maxAnimations) {
+    //       $('.site-logo').off('mouseout');
+    //       return false;
+    //     }
+
+    //     $('.site-title').velocity('finish').velocity({
+    //       left: '0'
+    //     }, 500);
+
+    //     $('.site-logo svg').velocity('finish').velocity({
+    //       translateX: '0',
+    //       rotateZ: '-1440deg'
+    //     }, 500);
+
+    //     animationCount++;
+    //   }, 300));
   }
 
 })();
