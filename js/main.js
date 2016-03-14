d1671cb4bb27a15dc7d631055d55737b95357ab3
Matti Salokangas <matti.salokangas@gmail.com>
2016-03-13 18:34:03 -0700

    Add social sharing options

--- js/main.js
+++ js/main.js
@@ -25,7 +25,13 @@
 
   window.sturdy = window.sturdy || {};
   window.sturdy.scrollBeast = new ScrollBeast();
-  window.sturdy.positionNavigation = positionNavigation;
+  window.sturdy.onScroll = onScroll;
+  window.sturdy.onResize = onResize;
+  window.sturdy.hints = {
+    isLaptop: function() {
+      return $(window).width() > 1024;
+    }
+  }
 
   // Initialization
 
@@ -45,13 +51,13 @@
       .on('click', function() {
         $('.js-site-nav,.js-site-nav-trigger,.js-site-title-wrapper,.js-site-header,.js-site-content')
           .toggleClass('active');
-        // sturdy.positionNavigation();
       });
 
-    // $(window).on('scroll', _.debounce(sturdy.positionNavigation, 50));
-    // $(window).on('resize', _.debounce(sturdy.positionNavigation, 50));
-    //
-    // sturdy.positionNavigation();
+    $(window).on('scroll', _.debounce(sturdy.onScroll, 100));
+    $(window).on('resize', _.debounce(sturdy.onResize, 100));
+
+    updateLogo();
+    positionNavigation();
   }
 
   function initTextAnimations() {
@@ -60,23 +66,52 @@
     });
   }
 
+  function onScroll() {
+    updateLogo();
+    positionNavigation();
+  }
+
+  function onResize() {
+    updateLogo();
+    positionNavigation();
+  }
+
   // Private
 
-  function positionNavigation() {
-    // Return if laptop
-    if ($(window).width() > 1024) {
+  function updateLogo() {
+    if (!sturdy.hints.isLaptop()) {
       return false;
     }
 
-    var top = 0;
-
-    if ($('.js-site-nav.active').length !== 0) {
-      var isAtTop, offset;
-
-      isAtTop = window.scrollY <= 156;
-      offset = isAtTop ? 156 : 20;
-      top = window.scrollY + offset;
+    var pos = getPositionInfo();
+    var postHeader = $('.post-header');
+    var postHeaderPos = postHeader.offset().top + postHeader.height();
+    if (pos.top > postHeaderPos) {
+      $('.js-site-title-wrapper').addClass('dark');
     }
+    else {
+      $('.js-site-title-wrapper').removeClass('dark');
+    }
+  }
+
+  function getPositionInfo() {
+    var headerOffset = 156;
+    var isAtTop = window.scrollY <= headerOffset;
+    var offset = isAtTop ? headerOffset : 20;
+
+    return {
+      isAtTop: isAtTop,
+      offset: offset,
+      top: window.scrollY + offset
+    };
+  }
+
+  function positionNavigation() {
+    if (sturdy.hints.isLaptop()) {
+      return false;
+    }
+
+    var top = window.scrollY;
 
     $('.js-site-nav').velocity('stop')
       .velocity({
